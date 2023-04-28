bf69bdfea2fdad91e8d309a3b17ab036df095996
Olifan <olifananton@gmail.com>
2023-04-28 19:52:20 +0300

    Add basic UI to CityViewPage

--- src/pages/CityViewPage/CityViewPage.tsx
+++ src/pages/CityViewPage/CityViewPage.tsx
@@ -4,9 +4,19 @@ import styles from './CityViewPage.module.css';
 interface CityViewPageProps {}
 
 const CityViewPage: FC<CityViewPageProps> = () => (
-  <div className={styles.CityViewPage}>
-    CityViewPage Component
+  <div>
+  <div className={styles.container}>
+    <div className={styles.header}>
+      <h1>Simple Weather App</h1>
+    </div>
   </div>
+
+  <div className={styles.container}>
+    <div className={styles.footer}>
+      <p>Made with <span>❤</span> by Anton Olifan</p>
+    </div>
+  </div>
+</div>
 );
 
 export default CityViewPage;
