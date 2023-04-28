819321cbb415c5c363761196353092fd5f3fe4f9
Olifan <olifananton@gmail.com>
2023-04-28 20:46:39 +0300

    Fix Prettier

--- src/pages/CityViewPage/CityViewPage.tsx
+++ src/pages/CityViewPage/CityViewPage.tsx
@@ -1,22 +1,24 @@
-import React, { FC } from 'react';
-import styles from './CityViewPage.module.css';
+import React, { FC } from "react";
+import styles from "./CityViewPage.module.css";
 
 interface CityViewPageProps {}
 
 const CityViewPage: FC<CityViewPageProps> = () => (
-  <div>
-  <div className="container">
-    <div className="header">
-      <h1>Simple Weather App</h1>
-    </div>
-  </div>
+	<div>
+		<div className="container">
+			<div className="header">
+				<h1>Simple Weather App</h1>
+			</div>
+		</div>
 
-  <div className="container">
-    <div className="footer">
-      <p>Made with <span>❤</span> by Anton Olifan</p>
-    </div>
-  </div>
-</div>
+		<div className="container">
+			<div className="footer">
+				<p>
+					Made with <span>❤</span> by Anton Olifan
+				</p>
+			</div>
+		</div>
+	</div>
 );
 
 export default CityViewPage;
