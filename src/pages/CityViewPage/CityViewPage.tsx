e624f34db0c6a46497029aafbcc570063b2ce4e9
Olifan <olifananton@gmail.com>
2023-04-28 19:43:15 +0300

    Create CityViewPage

---
+++ src/pages/CityViewPage/CityViewPage.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import styles from './CityViewPage.module.css';
+
+interface CityViewPageProps {}
+
+const CityViewPage: FC<CityViewPageProps> = () => (
+  <div className={styles.CityViewPage}>
+    CityViewPage Component
+  </div>
+);
+
+export default CityViewPage;
