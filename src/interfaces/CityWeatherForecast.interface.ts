45f4fdf6e561815f82ca55880ee6164bba0b5274
Olifan <olifananton@gmail.com>
2023-05-03 23:58:45 +0300

    Create Interface

--- src/interfaces/CityWeatherForecast.interface.ts
+++ src/interfaces/CityWeatherForecast.interface.ts
@@ -1,17 +1,42 @@
-interface CityListPage {
+interface CityWeatherForecast {
+    coord: {
+      lon: number;
+      lat: number;
+    };
+    weather: {
+      id: number;
+      main: string;
+      description: string;
+      icon: string;
+    }[];
+    base: string;
+    main: {
+      temp: number;
+      feels_like: number;
+      temp_min: number;
+      temp_max: number;
+      pressure: number;
+      humidity: number;
+    };
+    visibility: number;
+    wind: {
+      speed: number;
+      deg: number;
+    };
+    clouds: {
+      all: number;
+    };
+    dt: number;
+    sys: {
+      type: number;
+      id: number;
+      country: string;
+      sunrise: number;
+      sunset: number;
+    };
+    timezone: number;
+    id: number;
     name: string;
-    country: string;
-    temp: number;
-    icon: string;
-    description: string;
-}
-
-interface CityViewPage{
-    name: string;
-    country: string;
-    temp: number;
-    feels_like: number;
-    description: string;
-    temp_max: number;
-    temp_min: number;
-}
+    cod: number;
+  }
+  
