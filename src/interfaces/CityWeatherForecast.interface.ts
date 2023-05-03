594d7918e164c5c44ef3b3db2330e96faa3e143b
Olifan <olifananton@gmail.com>
2023-05-04 00:53:57 +0300

    Make interface with inserted

--- src/interfaces/CityWeatherForecast.interface.ts
+++ src/interfaces/CityWeatherForecast.interface.ts
@@ -1,39 +1,51 @@
-interface CityWeatherForecast {
-    coord: {
-      lon: number;
-      lat: number;
-    };
-    weather: {
-      id: number;
-      main: string;
-      description: string;
-      icon: string;
-    }[];
+  interface Coord {
+    lon: number;
+    lat: number;
+  }
+  
+  interface Weather {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }
+  
+  interface Main {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+  }
+  
+  interface Wind {
+    speed: number;
+    deg: number;
+  }
+  
+  interface Clouds {
+    all: number;
+  }
+  
+  interface Sys {
+    type: number;
+    id: number;
+    country: string;
+    sunrise: number;
+    sunset: number;
+  }
+  
+  interface CityWeatherForecast {
+    coord: Coord;
+    weather: Weather[];
     base: string;
-    main: {
-      temp: number;
-      feels_like: number;
-      temp_min: number;
-      temp_max: number;
-      pressure: number;
-      humidity: number;
-    };
+    main: Main;
     visibility: number;
-    wind: {
-      speed: number;
-      deg: number;
-    };
-    clouds: {
-      all: number;
-    };
+    wind: Wind;
+    clouds: Clouds;
     dt: number;
-    sys: {
-      type: number;
-      id: number;
-      country: string;
-      sunrise: number;
-      sunset: number;
-    };
+    sys: Sys;
     timezone: number;
     id: number;
     name: string;
