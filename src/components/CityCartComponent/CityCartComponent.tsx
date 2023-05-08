036eff32b01f36dd2036a55d2ddeadc114a0b864
Olifan <olifananton@gmail.com>
2023-05-08 18:26:11 +0300

    Fix css

--- src/components/CityCartComponent/CityCartComponent.tsx
+++ src/components/CityCartComponent/CityCartComponent.tsx
@@ -3,17 +3,17 @@ import styles from "./CityCartComponent.module.scss";
 
 const CityCartComponent = () => {
 	return (
-		<div className="cards">
-			<div className="city">
+		<div className={styles.cards}>
+			<div className={styles.city}>
 				<span>Kyiv</span>
 				<sup>UA</sup>
 			</div>
-			<div className="temp">
+			<div className={styles.temp}>
 				13
 				<sup>°C</sup>
 			</div>
-			<div className="weather">
-				<img className="icon" src="" alt="icon" />
+			<div className={styles.weather}>
+				<img className={styles.icon} src="" alt="icon" />
 				<span>Sunny</span>
 			</div>
 		</div>
